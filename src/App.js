eb73e522bf886242f4f4025ff3cc97e5aa92aa47
Kiemonek <ytoconner@gmail.com>
2024-05-15 21:20:36 +0200

    changed style

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <meta/>
+        <meta />
         <Header />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,30 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 export default function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar className= "Header-appbar" position="static">
+      <AppBar className="Header-appbar" position="static">
         <Toolbar variant="dense">
-          <Typography variant="h3" color="inherit" component="div" sx={{ flexGrow: 1 }}>
-            <b>
-              Recipe finder
-            </b>
+          <Typography
+            variant="h3"
+            color="inherit"
+            component="div"
+            sx={{ flexGrow: 1 }}
+          >
+            <b>Recipe finder</b>
           </Typography>
         </Toolbar>
         <Toolbar variant="dense">
-          <Typography variant="h6" color="inherit" component="div" sx={{ flexGrow: 1 }}>
+          <Typography
+            variant="h6"
+            color="inherit"
+            component="div"
+            sx={{ flexGrow: 1 }}
+          >
             Find the best recipes from around the world!
           </Typography>
         </Toolbar>
