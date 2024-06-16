96621201b85624d1d355fd2df448ce64a7bb2c68
Kiemonek <ytoconner@gmail.com>
2024-06-16 22:38:39 +0200

    chore: Update .gitignore and App.js, and remove RecipeList component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
 import "./App.css";
-import React, { useState } from "react";
-import Navbar from "./components/Navbar";
-import SearchBar from "./components/SearchBar";
-import RecipeList from "./components/RecipeList";
+import { Searchbar } from "./components/Searchbar.js";
+import Navbar from "./components/Navbar.js";
+import { useState } from "react";
+import { RecipesList } from "./components/RecipesList.js";
 
-// function App() {
-//   const [recipes, setRecipes] = useState([]);
-//   console.log(recipes);
-
-//   return (
-//     <div className="App">
-//       <Navbar />
-//       <SearchBar onSearch={setRecipes} />
-//     </div>
-//   );
-// }
-
-const App = () => {
+function App() {
   const [recipes, setRecipes] = useState([]);
-
-  const searchRecipes = async (query) => {
-    const response = await fetch(
-      `https://api.edamam.com/search?q=${query}&app_id=29571d4a&app_key=63d060efa6a9df1a240486514bdeb7b2`,
-    );
-    setRecipes(response.data.hits);
-  };
-
+  console.log(recipes);
   return (
     <div className="App">
       <Navbar />
-      <SearchBar onSearch={searchRecipes} />
-      <RecipeList recipes={recipes} />
+      <Searchbar setRecipes={setRecipes} />
+      <RecipesList recipes={recipes} />
     </div>
   );
-};
+}
 
 export default App;
